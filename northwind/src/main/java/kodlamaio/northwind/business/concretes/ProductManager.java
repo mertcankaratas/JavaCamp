372614c7a8231e097ee9fddbb3e42a8990968029
Mertcan Karataş <can.mert.karatas@gmail.com>
2022-09-19 16:46:23 +0300

    Day 9

--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -15,6 +15,7 @@ import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
 import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 @Service
 public class ProductManager implements ProductService {
@@ -107,6 +108,13 @@ public class ProductManager implements ProductService {
 		
 		Sort sort = Sort.by(Sort.Direction.DESC,"productName");
 		return new SuccessDataResult<List<Product>>(this.productDao.findAll(sort),"Başarılı");
+	}
+
+
+	@Override
+	public DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails() {
+		// TODO Auto-generated method stub
+		return new SuccessDataResult<List<ProductWithCategoryDto>>(this.productDao.getProductWithCategoryDetails(),"Data Listelendi");
 	}
 
 }
